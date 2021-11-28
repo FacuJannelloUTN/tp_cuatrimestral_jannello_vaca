3a5eed9b4e2151464e4e140eb22a7d5dc8a7b6d4
Facundo Jannello <facundojannellobaldi@gmail.com>
2021-11-28 15:05:47 -0300

    Cambios generales

--- tp_cuatrimestral_jannello_vaca/Products.aspx.designer.cs
+++ tp_cuatrimestral_jannello_vaca/Products.aspx.designer.cs
@@ -58,5 +58,50 @@ namespace tp_cuatrimestral_jannello_vaca
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox products_precioMaximo;
+
+        /// <summary>
+        /// products_buscar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox products_buscar;
+
+        /// <summary>
+        /// products_categorias control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList products_categorias;
+
+        /// <summary>
+        /// products_marca control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList products_marca;
+
+        /// <summary>
+        /// products_precioMinimo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox products_precioMinimo;
+
+        /// <summary>
+        /// products_precioMaximo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox products_precioMaximo;
     }
 }
